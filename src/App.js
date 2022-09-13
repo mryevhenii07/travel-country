006798ddb9b645997a04910180e70a1c054ee734
mryevhenii07 <mrit.works07@gmail.com>
2022-09-13 20:17:47 +0300

    create search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Destinations from "./components/Destinations/Destinations";
+import Search from "./components/Search/Search";
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
       <Destinations />
+      <Search />
     </div>
   );
 }
--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import s from "./Destinations.module.css";
 
+import BoraBora from "../../assets/borabora.jpg";
+import BoraBora2 from "../../assets/borabora2.jpg";
+import Maldives from "../../assets/maldives.jpg";
+import Maldives2 from "../../assets/maldives2.jpg";
+import KeyWest from "../../assets/keywest.jpg";
+
 const Destinations = () => {
-  return <div>Destinations</div>;
+  return (
+    <div className={s.destinations}>
+      <div className={s.container}>
+        <h1>All-Inclusive Resorts</h1>
+        <p>On the Caribbeans Best Beaches</p>
+        <div className={`${s.imgContainer} `}>
+          <img
+            className={`${s.spanThree} ${s.imageGridRowTwo}`}
+            src={BoraBora}
+            alt="#"
+          />
+          <img src={BoraBora2} alt="#" />
+          <img src={Maldives} alt="#" />
+          <img src={Maldives2} alt="#" />
+          <img src={KeyWest} alt="#" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Destinations;
